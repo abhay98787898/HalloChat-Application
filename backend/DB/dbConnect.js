4ee0dec323bff1ce0e4eafb11757c309e57b49f2
Abhay Pratap Singh <abhi98787898@gmail.com>
2025-11-17 22:39:56 +0530

    HalloChat application

---
+++ backend/DB/dbConnect.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const dbConnect = async()=>{
+    try {
+        await mongoose.connect(process.env.MONGODB_CONNECT),
+        console.log("DataBase Connected Successfully");
+    } catch (error) {
+        console.log(console.error);
+    }
+}
+ 
+
+export default dbConnect
